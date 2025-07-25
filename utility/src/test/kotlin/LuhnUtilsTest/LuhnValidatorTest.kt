b764a4d61e599cc30db7a5143cfef4b5791b08a7
nadineislamm <nadine.islam@ebetech.com.eg>
2025-07-25 22:59:35 +0300

    Add unit tests for luhn validation

---
+++ utility/src/test/kotlin/LuhnUtilsTest/LuhnValidatorTest.kt
@@ -0,0 +1,31 @@
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import utils.isLuhnValid
+
+class LuhnValidatorTest {
+
+    @Test
+    fun `when Visa card number is valid, isLuhnValid should returns true`() {
+        val cardNumber = "4539 1488 0343 6467"
+        assertTrue(isLuhnValid(cardNumber))
+    }
+
+    @Test
+    fun `when Mastercard number is valid, isLuhnValid should returns true`() {
+        val cardNumber = "5555 5555 5555 4444"
+        assertTrue(isLuhnValid(cardNumber))
+    }
+
+    @Test
+    fun `when card number is invalid, isLuhnValid should returns false`() {
+        val cardNumber = "5555 5555 5555 4444"
+        assertTrue(isLuhnValid(cardNumber))
+    }
+
+    @Test
+    fun `when non-digits are entered, isLuhnValid should ignore it and return true`() {
+        val cardNumber = "4539-1488-0343-6467"
+        assertTrue(isLuhnValid(cardNumber))
+    }
+
+}
