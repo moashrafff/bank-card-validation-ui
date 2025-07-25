da5006ba6117e310f1f0029cf1f7107049a9f9d6
nadineislamm <nadine.islam@ebetech.com.eg>
2025-07-25 21:47:55 +0300

    - Add Card Types - Apply Luhn Algorithm

---
+++ utility/src/main/kotlin/enums/CardTypes.kt
@@ -0,0 +1,11 @@
+package enums
+
+enum class CardTypes {
+    VISA,
+    MASTERCARD,
+    AMERICAN_EXPRESS,
+    DISCOVER,
+    UNION_PAY,
+    JCB,
+    DINERS_CLUB
+}
---
+++ utility/src/main/kotlin/utils/LuhnUtils.kt
@@ -0,0 +1,10 @@
+package utils
+
+fun isLuhnValid(cardNumber: String): Boolean {
+    val reversedNumber = cardNumber.filter { it.isDigit() }.reversed()
+    val totalSum = reversedNumber.mapIndexed { index, digit ->
+        val value = digit.digitToInt() * (if (index % 2 == 0) 1 else 2)
+        if (value > 9) value - 9 else value
+    }.sum()
+    return totalSum % 10 == 0
+}
