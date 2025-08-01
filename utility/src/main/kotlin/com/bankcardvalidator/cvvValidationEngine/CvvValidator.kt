6e92d87bb0c970acfcd91dab019b9be69408cf1e
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-08-01 15:24:50 +0300

    formating the code

--- utility/src/main/kotlin/com/bankcardvalidator/cvvValidationEngine/CvvValidator.kt
+++ utility/src/main/kotlin/com/bankcardvalidator/cvvValidationEngine/CvvValidator.kt
@@ -3,8 +3,11 @@ package com.bankcardvalidator.cvvValidationEngine
 import com.bankcardvalidator.brandDetectorEngine.model.CardBrandRules
 import com.bankcardvalidator.cvvValidationEngine.result.CvvValidationResult
 
-fun isCvvValid(cardNumber: String, cvv: String): CvvValidationResult =when{
+fun isCvvValid(cardNumber: String, cvv: String): CvvValidationResult = when {
     !cvv.all { it.isDigit() } -> CvvValidationResult.InvalidFormat
-    CardBrandRules.getCvvLength(cardNumber) == null || cvv.length != CardBrandRules.getCvvLength(cardNumber) -> CvvValidationResult.InvalidLength
+    CardBrandRules.getCvvLength(cardNumber) == null || cvv.length != CardBrandRules.getCvvLength(
+        cardNumber
+    ) -> CvvValidationResult.InvalidLength
+
     else -> CvvValidationResult.Valid
 }
