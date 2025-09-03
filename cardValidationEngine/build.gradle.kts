420b288816b48a7fa7eead6627dc0709a7715b62
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-09-03 11:45:35 +0300

    release 1.0.0

--- cardValidationEngine/build.gradle.kts
+++ cardValidationEngine/build.gradle.kts
@@ -1,26 +1,11 @@
 plugins {
-    id("java-library")
+    `java-library`
     alias(libs.plugins.jetbrains.kotlin.jvm)
-    id("maven-publish")
+    `maven-publish`
 }
 
 group = "com.bankcardvalidator"
 version = "1.0.0"
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            artifactId = "cardvalidatorengine"
-
-            from(components["java"])
-
-            pom {
-                name.set("cardvalidatorengine")
-                description.set("Card validation engine.")
-            }
-        }
-    }
-}
 
 java {
     toolchain { languageVersion.set(JavaLanguageVersion.of(11)) }
@@ -33,16 +18,14 @@ kotlin {
     }
 }
 
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+        }
     }
 }
+
 dependencies {
     testImplementation(libs.junit)
 }
